9cca3c2d825c411544d10b8d16b2e1b961423a25
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2024-12-08 00:26:04 +0100

    Move Keyboard to Handwired Directory

--- keyboards/handwired/monochorder/keymaps/default/keymap.c
+++ keyboards/handwired/monochorder/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include "print.h"
 #include "action.h"
 #include QMK_KEYBOARD_H
-#include "keyboards/monochorder/lib/chorder_logic.h"
+#include "../../lib/chorder_logic.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
